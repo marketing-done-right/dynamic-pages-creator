75d30ee28449198a8a806b5ebd860a7a44f35516
Hans Steffens <hans@steffens.co>
2024-04-12 12:14:19 -0500

    Initial Files

---
+++ dynamic-pages-seo.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Dynamic Pages with SEO
+ * Description: Automatically generates web pages based on predefined URL slugs and dynamically assigns SEO meta tags to each page, tailored to its slug, to improve search engine visibility.
+ * Version: 1.0
+ * Author: Hans Steffens & Marketing Done Right LLC
+ * Author URI: https://marketingdr.co
+ * License: GPL v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
